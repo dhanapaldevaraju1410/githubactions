3860fd36ae99d649b8f329aede912c1e6ae06962
dhanapaldevaraju1410 <dhanapal.devaraju@atyeti.com>
2025-04-24 09:33:55 +0530

    Create ec2.tf

---
+++ ec2.tf
@@ -0,0 +1,22 @@
+
+provider "google" {
+  project = "YOUR_PROJECT_ID"
+  region  = "YOUR_REGION"
+}
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "my-vm"
+  machine_type = "n1-standard-1"
+  zone         = "us-central1-a"
+
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-minimal-2210-kinetic-amd64-v20230126"
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {}
+  }
+}
